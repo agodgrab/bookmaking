2b1b6ca318b920b28a7a596a9f747d38fb049bbb
agodgrab <a.lottia@o2.pl>
2020-01-02 10:42:34 +0100

    Spring configuration added

--- src/main/java/org/agodgrab/App.java
+++ src/main/java/org/agodgrab/App.java
@@ -1,7 +1,11 @@
 package org.agodgrab;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("A");
+        SpringApplication.run(App.class, args);
     }
 }
